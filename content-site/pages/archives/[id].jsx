8a93c0e4a49b03f5d3a833bef6db35033d289a14
Chris Cressman <chris@chriscressman.com>
2023-02-16 17:37:29 -0500

    Make archives pages dynamic

--- content-site/pages/archives/[id].jsx
+++ content-site/pages/archives/[id].jsx
@@ -1,23 +1,43 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import Header from '../components/header'
-import Footer from '../components/footer'
-import ArticlesList from '../components/articles-list'
+import Header from '../../components/header'
+import Footer from '../../components/footer'
+import ArticlesList from '../../components/articles-list'
 
-export default function ArchivePage () {
+export default function ArchivePage ({ archive }) {
   return (
     <>
       <Head>
-        <title>Archive</title>
+        <title>{archive.id}</title>
       </Head>
 
       <Header />
 
-      <h1>Archive</h1>
+      <h1>{archive.id}</h1>
 
       <ArticlesList />
 
       <Footer />
     </>
-  )
+  );
+}
+
+export async function getStaticPaths () {
+  return {
+    paths: [
+      { params: { id: '2022' } },
+      { params: { id: '2023' } }
+    ],
+    fallback: false
+  };
+}
+
+export async function getStaticProps ({ params }) {
+  const getArchive = function (id) {
+    return { id: id };
+  };
+
+  return {
+    props: { archive: getArchive(params.id) }
+  };
 }
--- content-site/pages/archives/index.jsx
+++ content-site/pages/archives/index.jsx
@@ -1,9 +1,26 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import Header from '../components/header'
-import Footer from '../components/footer'
+import Header from '../../components/header'
+import Footer from '../../components/footer'
 
-export default function ArchivesPage () {
+export default function ArchivesPage ({ archives }) {
+  const getUrl = function (id) {
+    return {
+      pathname: '/archives/[id]',
+      query: { id: id },
+    };
+  };
+
+  const getListItem = function ({ id }) {
+    return (
+      <li key={id}>
+        <Link href={getUrl(id)}>{id}</Link>
+      </li>
+    );
+  };
+
+  const listItems = archives.map(getListItem);
+
   return (
     <>
       <Head>
@@ -13,16 +30,22 @@ export default function ArchivesPage () {
       <Header />
 
       <h1>Archives</h1>
-      <ul>
-        <li>
-          <Link href="/archive">Archive</Link>
-        </li>
-        <li>
-          <Link href="/archive">Archive</Link>
-        </li>
-      </ul>
+      <ul>{listItems}</ul>
 
       <Footer />
     </>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const getArchives = function () {
+    return [
+      { id: '2022' },
+      { id: '2023' }
+    ];
+  };
+
+  return {
+    props: { archives: getArchives() }
+  };
 }
