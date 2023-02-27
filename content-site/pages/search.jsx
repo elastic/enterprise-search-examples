7303c5e44558a409b4f5c898a0eb8534887dabf7
Chris Cressman <chris@chriscressman.com>
2023-02-27 16:14:38 -0500

    Refactor static search page

--- content-site/pages/search.jsx
+++ content-site/pages/search.jsx
@@ -1,16 +1,43 @@
+import { useEffect } from 'react'
 import Head from 'next/head'
+import Link from 'next/head'
 
-import Header from '../components/header.jsx'
 import Footer from '../components/footer.jsx'
 
 export default function SearchPage () {
+  //const [data, setData] = useState(null)
+
+  useEffect(() => {
+    //setLoading(true)
+    //fetch('/api/profile-data')
+      //.then((res) => res.json())
+      //.then((data) => {
+        //setData(data)
+        //setLoading(false)
+      //})
+    console.log('using effect');
+  }, [])
+
   return (
     <>
       <Head>
         <title>Search</title>
       </Head>
 
-      <Header />
+      <ul className="pages-nav">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/articles">Articles</Link>
+        </li>
+      </ul>
+
+      <p className="search-bar">
+        <input type="search" />
+        &nbsp;
+        <input type="submit" value="Search" />
+      </p>
 
       <h1>Search</h1>
 
